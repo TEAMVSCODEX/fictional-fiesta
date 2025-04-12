627a53a83c1e57c8c0b9710072f709a8c34dcad1
JetQuintana <1497728@senati.pe>
2025-04-12 14:24:08 -0500

    Insercion de datos

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
     fetch("data.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data["productos"]);
-    })
-    .catch(error => {
-        console.error('Error al cargar los productos:', error);
-      });
-})
+        .then(response => response.json())
+        .then(data => {
+            const productos = data["productos"];
+            const productosContainer = document.querySelector("#prod"); 
+
+            productos.forEach(producto => {
+                const productoHTML = `
+                    <div class="col">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h5 class="card-title">${producto.nombre}</h5>
+                                <p class="card-text">Marca: ${producto.marca}</p>
+                                <p class="card-text">Precio: S/${producto.precio}</p>
+                                <p class="card-text">Presentación: ${producto.presentacion}</p>
+                                <p class="card-text">Stock: ${producto.stock}</p>
+                                <p class="card-text">Cantidad: ${producto.cantidad}</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                productosContainer.innerHTML += productoHTML;
+            });
+        })
+        .catch(error => {
+            console.error('Error al cargar los productos:', error);
+        });
+});
